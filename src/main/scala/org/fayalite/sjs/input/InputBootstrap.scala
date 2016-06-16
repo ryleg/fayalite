c1ca89b2cb11af659146bdf47a5aa7f83f8d7fb3
ryle <ryledup@gmail.com>
2016-06-16 09:41:29 -0700

    Getting rid of ellipses tests, too unwieldy

--- src/main/scala/org/fayalite/sjs/input/InputBootstrap.scala
+++ src/main/scala/org/fayalite/sjs/input/InputBootstrap.scala
@@ -113,39 +113,7 @@ trait TileCoordinator {
   }
 
   val wordResolve = mutable.HashMap[LatCoord, Array[CanvasContextInfo]]()
-  val wordBubbleResolve = mutable.HashMap[LatCoord, CanvasContextInfo]()
 
-  def drawEllipse(
-                   wordLen: Int, origin: LatCoord, bubbleCanvas: CanvasContextInfo
-                 ) = {
-    val offsetLeft = 1
-    val offsetTop =  .5D
-    val x = origin.x - (offsetLeft*minSize)
-    val y = origin.y - offsetTop*minSize
-    // Fix alpha calls to split background filler out, below is hack
-    val kappa = .5522848
-    val width = (wordLen+2)*minSize
-    val height = minSize*2
-    val ox = (width / 2) * kappa // control point offset horizontal
-    val oy = (height / 2) * kappa // control point offset vertical
-    val xe = x + width           // x-end
-    val ye = y + height           // y-end
-    val xm = x + width / 2      // x-middle
-    val ym = y + height / 2   // y-middle
-    val ctx = bubbleCanvas.context
-    ctx.fillStyle = methodGold
-    ctx.strokeStyle = methodGold
-    ctx.globalAlpha = .41D
-    ctx.lineWidth = 2D
-    ctx.moveTo(x, ym)
-    ctx.bezierCurveTo(x, ym - oy, xm - ox, y, xm, y)
-    ctx.bezierCurveTo(xm + ox, y, xe, ym - oy, xe, ym)
-    ctx.bezierCurveTo(xe, ym + oy, xm + ox, ye, xm, ye)
-    ctx.bezierCurveTo(xm - ox, ye, x, ym + oy, x, ym)
-    //ctx.closePath(); // not used correctly, see comments (use to close off open path)
-    ctx.stroke()
-
-  }
 
   def mkWord(word: String, origin: LatCoord) = {
     val tempCnvRenders = word.map{_.toString}.map{mkCharLikeSquareNode}
@@ -168,10 +136,8 @@ trait TileCoordinator {
     val bubbleCanvas = createCanvasZeroSquare(bulkSize*2, zIndex = 2, alpha = 0D).copy(
       text = Some(word)
     )
-    drawEllipse(len, origin, bubbleCanvas)
 
     wordResolve(origin) = tempCnvRenders.toArray
-    wordBubbleResolve(origin) = bubbleCanvas
   }
 
 }
@@ -263,11 +229,7 @@ with TileCoordinator {
         mHover.move(me.tileCoordinates(minSize))
         bHover.move(me.tileCoordinates(bulkSize))
 
-        wordBubbleResolve.get(mHover.location).foreach{
-          q =>
-          //  q.clear()
-            println("Hover over word")
-        }
+
       }
     }
 }
